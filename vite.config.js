ed9097525c6ba6dfe09f19014a1e785c52858663
NikitaEfimovIT <nikita.efimov.it@gmail.com>
2025-03-03 16:19:38 +0300

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "breathing-better",
+  base: "https://github.com/NikitaEfimovIT/breathing-better",
 });
