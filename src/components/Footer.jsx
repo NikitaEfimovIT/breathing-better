c7021722711bafca0685160eba9555f4b98aed80
NikitaEfimovIT <nikita.efimov.it@gmail.com>
2025-03-04 18:42:39 +0300

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import { Box, Button, useMediaQuery } from "@mui/material";
-import { Text, TitleH1 } from "./baseComponents.js";
+import { Text, TitleH1, TitleH3 } from "./baseComponents.js";
+import { Link } from "react-scroll";
 
 export const Footer = () => {
   const isMdDown = useMediaQuery((theme) => theme.breakpoints.down(1200));
@@ -22,7 +23,7 @@ export const Footer = () => {
           justifyContent={"space-between"}
           flexDirection={isXsDown ? "column" : "row"}
           alignItems={"start"}
-          width={"100%"}
+          minWidth={"100%"}
         >
           <Box
             display={"flex"}
@@ -40,14 +41,29 @@ export const Footer = () => {
                 </Text>
               )}
             </Box>
-            <Button style={{ marginTop: 0, color: "white" }}>Why breathing?</Button>
-            <Button style={{ marginTop: 0, color: "white" }}>Our app</Button>
-            <Button style={{ marginTop: 0, color: "white" }}>Get an app</Button>
-          </Box>
-          <Box display={"flex"} flexDirection={"column"} alignItems={"start"} justifyContent={"start"}>
-            <Button variant={"contained"} style={{ height: "40px", fontSize: "17px", borderRadius: 15 }}>
-              Try it now!
+            <Button style={{ marginTop: 0, color: "white" }}>
+              <Link to="section2" smooth={true} duration={500}>
+                Why breathing?
+              </Link>
             </Button>
+            <Button style={{ marginTop: 0, color: "white" }}>
+              <Link to="section3" smooth={true} duration={500}>
+                Our app
+              </Link>
+            </Button>
+            <Button style={{ marginTop: 0, color: "white" }}>
+              {" "}
+              <Link to="section4" smooth={true} duration={500}>
+                About us
+              </Link>
+            </Button>
+          </Box>
+          <Box display={"flex"} flexDirection={"column"} alignItems={"start"} justifyContent={"start"} color={"white"}>
+            <TitleH3 sx={{ color: "white", marginBottom: 0 }}>Contacts:</TitleH3>
+            <TitleH3 sx={{ color: "white", margin: 0 }}>Our email</TitleH3>
+            <Text sx={{ color: "white", marginTop: 0 }}>breathe.better@contact.com</Text>
+            <TitleH3 sx={{ color: "white", margin: 0 }}>Our phone</TitleH3>
+            <Text sx={{ color: "white", marginTop: 0 }}>+49 (1525) 107 41 84</Text>
             {isMdDown && (
               <Text style={{ margin: 0, lineHeight: "normal", fontSize: 14, color: "white" }}>
                 {`@BreathBetter ${new Date().getFullYear()}.`}
